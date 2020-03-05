257640934bf7c074a0c0c0c5904fa63fc06291f3
Gokberk Seber <gokberkseber@hotmail.com>
2020-03-05 20:02:11 +0300

    KategoriListelendi

---
+++ MyBlogSite/Controllers/KategoriController.cs
@@ -0,0 +1,23 @@
+﻿using MyBlogSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogSite.Controllers
+{
+    public class KategoriController : Controller
+    {
+        BlogContext db = new BlogContext();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult KategoriWidget()
+        {
+            return PartialView(db.Kategori.ToList());
+        }
+    }
+}
