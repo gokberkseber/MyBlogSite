384b524d46c6beb38e30dc1d6d3a6c7e8df3d7c3
Gokberk Seber <gokberkseber@hotmail.com>
2020-03-05 20:01:57 +0300

    KategoriCrudYapildi

--- MyBlogSite/Areas/Admin/Controllers/KategoriController.cs
+++ MyBlogSite/Areas/Admin/Controllers/KategoriController.cs
@@ -1,6 +1,7 @@
 ﻿using MyBlogSite.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,7 +29,35 @@ namespace MyBlogSite.Areas.Admin.Controllers
             kat.KategoriAdi = model.KategoriAdi;
             db.Kategori.Add(kat);
             db.SaveChanges();
-            return RedirectToAction("KategoriEkle");
+            return RedirectToAction("KatListele");
+        }
+
+        public ActionResult KatListele()
+        {
+            List<Kategori> katList = db.Kategori.ToList();
+            return View(katList);
+        }
+
+        public ActionResult KatSil(int id)
+        {
+            Kategori silKat = db.Kategori.Find(id);
+            db.Kategori.Remove(silKat);
+            db.SaveChanges();
+            return RedirectToAction("KatListele");
+        }
+
+        public ActionResult KatGuncelle(int ? id)
+        {
+            var kat = db.Kategori.SingleOrDefault(k => k.KategoriID==id);
+            return View(kat);
+        }
+
+        [HttpPost]
+        public ActionResult KatGuncelle(Kategori model)
+        {
+            db.Entry(model).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("KatListele");
         }
     }
 }
